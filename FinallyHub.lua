7777e9a48f8bf7fd04c8e67ce5e7f53c04e5d66a
02Dcs <95237577+02Dcs@users.noreply.github.com>
2022-09-11 11:57:22 +0200

    Add files via upload

---
+++ FinallyHub.lua
@@ -0,0 +1,24 @@
+--print(game.PlaceId)
+
+local url = 'https://raw.githubusercontent.com/02Dcs/02-Hub/main';
+local b = game:GetService('Players')
+
+local gameswht = {
+    [9498006165] = 'Tapping Simulator',
+    [7267631004] = 'carry people simulator 3',
+    [8542259458] = 'SkyWars', [8542275097] = 'SkyWars',
+    [2990100290] = 'RPG Simulator',
+}
+
+if not table.find(gameswht, game.PlaceId) then 
+     b.LocalPlayer:Kick('Wrong Game!')
+end
+
+for z,k in next, gameswht do
+    gameswht[z] = table.concat(k:split(' '), '_')
+end
+
+local n = gameswht[game.PlaceId] or gameswht[game.GameId]
+return loadstring(game:HttpGet(url.. '/' ..n ..'.lua'))()
+--
+
